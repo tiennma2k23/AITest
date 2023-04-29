5d25765aab16178dae18fb0e2f92853af06939ca
tiennma2k23cvp <tiennma2k23@cvp.vn>
2023-04-30 02:18:00 +0700

    add file

--- UI/UI.py
+++ UI/UI.py
@@ -11,28 +11,25 @@ import sys
 
 
 
-class root(Tk):
-    def __init__(self, *args, **kwargs):
-        Tk.__init__(self, *args, **kwargs)
-        # Adding a title to the window
-        self.wm_title("Test Application")
-        self.geometry("800x480")
-        self.resizable(False, False)
+class UserHandle(Frame):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
 
-        # creating a frame and assigning it to container
-        container = Frame(self)
-        # specifying the region where the frame is packed in root
-        container.pack(side="top", fill="both", expand=True)
+        # Create a frame and assigning it to container
+        # container = Frame(self)
+        # Specify the region where the frame is packed in root
+        self.pack(side="top", fill="both", expand=True)
 
-        # configuring the location of the container using grid
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        # Configure container's location with grid
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
-        # We will now create a dictionary of frames
+        # Create a dictionary of frames
         self.frames = {}
         # we'll create the frames themselves later but let's add the components to the dictionary.
-        for F in (login_frame, sign_up_frame, resend):
-            frame = F(container, self)
+        for F in (login_frame, sign_up_frame,resend):
+            frame = F(self, self)
 
             # the windows class acts as the root window for the frames.
             self.frames[F] = frame
