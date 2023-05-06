f122eb9847f4dd316496e6d86584500523855b01
tiennma2k23cvp <tiennma2k23@cvp.vn>
2023-05-07 04:57:12 +0700

    fix

--- Database_processing/User_db/update_login_first.py
+++ Database_processing/User_db/update_login_first.py
@@ -6,7 +6,7 @@ uri = json.loads(open("adminAuth/adminAuth.json","r").read())["uri"]
 cluster=MongoClient(uri, server_api=ServerApi('1'))
 db=cluster['AI_db']
 collection=db['User_db']
-def update_img_profile_user(username,status):
+def update_login_first(username,status):
     collection=db['User_db']
     collection.update_one({'username':username},{'$set':{'first_login':status}})
     return True
