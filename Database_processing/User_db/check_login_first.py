bfa96016adda1d91b6d9b06a921bcd52a2a5e57a
Sukkaito <40479420+Sukkaito@users.noreply.github.com>
2023-05-07 11:04:13 +0700

    Add default value for login_first

--- Database_processing/User_db/check_login_first.py
+++ Database_processing/User_db/check_login_first.py
@@ -1,8 +1,12 @@
 from Database_processing.User_db.get import find_user
-
+from Database_processing.User_db.update_login_first import update_login_first
 
 def check_login_first(usr):
     __=find_user(usr)
     if (__['status']==False): return True
     else:
-        return __['data']['first_login']
+        try:
+            return __['data']['first_login']
+        except:
+            update_login_first(__['data']['username'], False)
+            return False
--- Database_processing/User_db/get_gendermale.py
+++ Database_processing/User_db/get_gendermale.py
@@ -1,9 +1,15 @@
 from Database_processing.User_db.get import find_user
-
+from Database_processing.User_db.update import update_user
 
 def get_gendermale(usr):
     __=find_user(usr)
     if (__['status']==False): return True
     else:
-        _status=__['data']['gendermale']
-        return _status
+        try:
+            _status=__['data']['gendermale']
+            return _status
+        except:
+            update_user(__['data']['username'], {
+                'gendermale': True
+            })
+            return True
